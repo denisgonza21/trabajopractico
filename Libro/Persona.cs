9370923fb9ffd77748430929e017bba55f349670
PabloV97 <54246205+PabloV97@users.noreply.github.com>
2019-09-15 14:30:46 -0400

    Agregue Persona

---
+++ Libro/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesBiblioteca
+{
+    class Persona
+    {
+        public int Id { get; set; }
+        public string NombreApellido { get; set; }
+        public int Telefono { get; set; }
+        public string TipoPersona { get; set; }
+    }
+}
