aa49687e6450461ff7bb0a99b35f955a3edd8257
Jaraadair <jaraadair@gmail.com>
2019-09-15 16:10:01 -0400

    Cambio Humberto Meza : agregar tabla de clientes

---
+++ Libro/Editorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesBiblioteca
+{
+   public class Editorial
+    {
+        // clase Editorial
+
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
