6d67dd57fb2c5695b1f91fc0954bc24c39fc04d1
denisgonza21 <denis.gonza21@gmail.com>
2019-09-15 11:29:33 -0400

    Agregar archivos de proyecto.

---
+++ Libro/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClasesBiblioteca
+{
+    public class Libro
+    {
+        public string Nombre { get; set; }
+        public string Editorial { get; set; }
+        public string Autor { get; set; }
+        public int cantidad_ejemplares { get; set; }
+        public int cantidad_disponible { get; set; }
+
+        public Libro() { }
+
+        public string ObtenerFichaCompleta()
+        {
+            return $"Nombre:{Nombre} Editorial:{Editorial} Autor:{Autor} Cant.Ejemplares:{cantidad_ejemplares} Cant.Disponible:{cantidad_disponible}";
+        }
+    }
+}
