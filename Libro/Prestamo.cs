5685ab9a9ac592150d0134b8ef296f70a1d66d38
PabloV97 <pablo.vera1997@gmail.com>
2019-09-15 17:05:36 -0400

    Agregue clase Prestamo

---
+++ Libro/Prestamo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasesBiblioteca
+{
+    public class Prestamo
+    {
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime FechaDevolucion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+    }
+}
