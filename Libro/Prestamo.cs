178a387889102a1e4b803ebc55f3b3ed9927d308
Jaraadair <jaraadair@gmail.com>
2019-10-21 00:04:11 -0300

    Modifique la clase Prestamo y el formulario del miso

--- Libro/Prestamo.cs
+++ Libro/Prestamo.cs
@@ -10,6 +10,31 @@ namespace ClasesBiblioteca
         public DateTime FechaDevolucion { get; set; }
         public DateTime FechaVencimiento { get; set; }
 
+        public static List<Prestamo> listaPrestamos = new List<Prestamo>();
+
+
+        public static void AgreagarPrestamo(Prestamo prestamo)
+        {
+            listaPrestamos.Add(prestamo);
+        }
+
+        public static void EliminarPrestamo(Prestamo prestamo)
+        {
+            listaPrestamos.Remove(prestamo);
+        }
+
+        public static List<Prestamo> ObtenerPrestamos()
+        {
+            return listaPrestamos;
+        }
+
+        public static void EditarPrestamo(Prestamo prestamo, int indice)
+        {
+
+            Prestamo.listaPrestamos[indice] = prestamo;
+        }
+
+
         public void DatosPrestamo() { }
     }
 }
--- ProyectoLibro/frmPrestamo.cs
+++ ProyectoLibro/frmPrestamo.cs
@@ -12,16 +12,80 @@ namespace ProyectoLibro
 {
     public partial class frmPrestamo : Form
     {
-        public frmPrestamo()
-        {
-            InitializeComponent();
-        }
-
-        private void frmPrestamo_Load(object sender, EventArgs e)
-        {
-
-        }
-
         
-    }
-}
+            string modo;
+            public frmPrestamo()
+            {
+                InitializeComponent();
+            }
+
+            private void frmPrestamo_Load(object sender, EventArgs e)
+            {
+                ActualizarPrestamos();
+            }
+
+            private void btnAgregar_Click(object sender, EventArgs e)
+            {
+                Prestamo prestamo = ObtenerDatosFormulario();
+                Prestamo.AgreagarPrestamo(prestamo);
+                ActualizarDatosLista();
+                LimpiarFormulario();
+            }
+
+
+            private void LimpiarFormulario()
+            {
+
+            }
+
+            private Prestamo ObtenerDatosFormulario()
+            {
+                Prestamo prestamo = new Prestamo();
+                prestamo.Cliente = (ClasesBiblioteca.Cliente)cboCliente.SelectedItem;
+                prestamo.Libro = (ClasesBiblioteca.Libro)cboLibro.SelectedItem;
+
+                prestamo.FechaPrestamo = dtpFechaPrestamo.Value.Date;
+                prestamo.FechaDevolucion = dtpFechaPrestamo.Value.Date;
+                prestamo.FechaVencimiento = dtpFechaVencimiento.Value.Date;
+
+                return Prestamo;
+
+            }
+
+
+            private void ActualizarDatosLista()
+            {
+                cboCliente.DataSource = null;
+                cboLibro.DataSource = null;
+            }
+
+
+            private void ActualizarPrestamos()
+            {
+                cboCliente.DataSource = null;
+                cboLibro.DataSource = null;
+            }
+
+            private void btnModificar_Click(object sender, EventArgs e)
+            {
+                modo = "EDITAR";
+            }
+
+            private void btnEliminar_Click(object sender, EventArgs e)
+            {
+                if (cboCliente.SelectedIndex > 0)
+                {
+                    Prestamo prestamo = (Prestamo)cboCliente.SelectedItem;
+
+                    Prestamo.listaPrestamos.Remove(prestamo);
+                    ActualizarPrestamos();
+                    LimpiarFormulario();
+                }
+                else
+                {
+                    MessageBox.Show("Favor seleccionar de la lista para eliminar");
+                }
+
+
+            }
+
