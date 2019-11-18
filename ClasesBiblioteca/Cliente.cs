1d7545198b9e1944175bbe745151651f940b8018
JazOjeda <jaz79791@gmail.com>
2019-11-17 23:27:44 -0300

    Realice la conexion con sql server del formulario cliente y codifique los botones del formulario.

--- ClasesBiblioteca/Cliente.cs
+++ ClasesBiblioteca/Cliente.cs
@@ -15,11 +15,132 @@ namespace ClasesBiblioteca
         public string nombre { get; set; }
         public string direccion { get; set; }
         public string telefono { get; set; }
-        public DateTime FechaInscripcion { get; set;}
+        public DateTime fecha_inscripcion { get; set;}
 
 
         public static List<Cliente> listaClientes = new List<Cliente>();
-        
+
+
+        public static void AgregarCliente(Cliente c)
+        {
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open(); //Abrimos la conex con la BD
+                string textoCmd = "insert into Cliente (nroDocumento, nombre, direccion, telefono, fecha_inscripcion) VALUES (@nroDocumento, @nombre, @direccion, @telefono, @fecha_inscripcion)";
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+                //PARAMETROS
+                SqlParameter c1 = new SqlParameter("@nroDocumento", c.nroDocumento);
+                SqlParameter c2 = new SqlParameter("@nombre", c.nombre);
+                SqlParameter c3 = new SqlParameter("@direccion", c.direccion);
+                SqlParameter c4 = new SqlParameter("@telefono", c.telefono);
+                SqlParameter c5 = new SqlParameter("@fecha_inscripcion", c.fecha_inscripcion);
+
+                //Le decimos a los parametros de que tipo de datos son
+                c1.SqlDbType = SqlDbType.VarChar;
+                c2.SqlDbType = SqlDbType.VarChar;
+                c3.SqlDbType = SqlDbType.VarChar;
+                c4.SqlDbType = SqlDbType.VarChar;
+                c5.SqlDbType = SqlDbType.DateTime;
+
+                //Agragamos los parametros al command
+                cmd.Parameters.Add(c1);
+                cmd.Parameters.Add(c2);
+                cmd.Parameters.Add(c3);
+                cmd.Parameters.Add(c4);
+                cmd.Parameters.Add(c5);
+                cmd.ExecuteNonQuery();
+
+            }
+
+
+
+        }
+
+        public static void EliminarCliente (Cliente c)
+        {
+            
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string SENTENCIA_SQL = "delete from cliente where id = @id";
+
+                SqlCommand cmd = new SqlCommand(SENTENCIA_SQL, con);
+                SqlParameter c6 = new SqlParameter("@id", c.id);
+                c6.SqlDbType = SqlDbType.Int;
+                cmd.Parameters.Add(c6);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void EditarCliente (int index, Cliente c)
+        {
+            //listaProveedores[index] = p;
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                string textoCMD = "UPDATE Cliente SET nroDocumento = @nroDocumento, nombre= @nombre, direccion = @direccion, telefono = @telefono, fecha_inscripcion = @fecha_inscripcion where id = @id";
+
+                SqlCommand cmd = new SqlCommand(textoCMD, con);
+
+                //DEFINICION DE PARAMETROS
+                SqlParameter c1 = new SqlParameter("@nroDocumento", c.nroDocumento);
+                SqlParameter c2 = new SqlParameter("@nombre", c.nombre);
+                SqlParameter c3 = new SqlParameter("@direccion", c.direccion);
+                SqlParameter c4 = new SqlParameter("@telefono", c.telefono);
+                SqlParameter c5 = new SqlParameter("@fecha_inscripcion", c.fecha_inscripcion);
+                SqlParameter c6 = new SqlParameter("@id", c.id);
+
+                //Le decimos a los parametros de que tipo de datos son
+                c1.SqlDbType = SqlDbType.VarChar;
+                c2.SqlDbType = SqlDbType.VarChar;
+                c3.SqlDbType = SqlDbType.VarChar;
+                c4.SqlDbType = SqlDbType.VarChar;
+                c5.SqlDbType = SqlDbType.DateTime;
+                c6.SqlDbType = SqlDbType.Int;
+
+
+                cmd.Parameters.Add(c1);
+                cmd.Parameters.Add(c2);
+                cmd.Parameters.Add(c3);
+                cmd.Parameters.Add(c4);
+                cmd.Parameters.Add(c5);
+                cmd.Parameters.Add(c6);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+        public static Cliente ObtenerCliente (int id)
+        {
+            Cliente cliente = null;
+
+            if (listaClientes.Count == 0)
+            {
+                Cliente.ObtenerClientes();
+            }
+
+            foreach (Cliente c in listaClientes)
+            {
+                if (c.id == id)
+                {
+                    cliente = c;
+                    break;
+                }
+            }
+
+            return cliente;
+        }
+
+
+
+
+
         public static List<Cliente> ObtenerClientes()
             
         {
@@ -27,7 +148,7 @@ namespace ClasesBiblioteca
             using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
             {
                 con.Open();
-                string textoCMD = "Select * from Cliente";
+                string textoCMD = "Select * from cliente";
                 SqlCommand cmd = new SqlCommand(textoCMD, con);
                 SqlDataReader electorDeDatos = cmd.ExecuteReader();
 
@@ -39,7 +160,7 @@ namespace ClasesBiblioteca
                     cliente.nroDocumento = electorDeDatos.GetString(2);
                     cliente.direccion = electorDeDatos.GetString(3);
                     cliente.telefono = electorDeDatos.GetString(4);
-                    cliente.FechaInscripcion = electorDeDatos.GetDateTime(5);
+                    cliente.fecha_inscripcion = electorDeDatos.GetDateTime(5);
 
                     listaClientes.Add(cliente);
                 }
@@ -49,6 +170,11 @@ namespace ClasesBiblioteca
                 return listaClientes;
         }
 
+        public override string ToString()
+        {
+           
+            return nombre;
+        }
 
     }
 }
--- ProyectoLibro/frmCliente.cs
+++ ProyectoLibro/frmCliente.cs
@@ -18,7 +18,109 @@ namespace ProyectoLibro
             InitializeComponent();
         }
 
-        
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            cliente.nroDocumento = txtDocumento.Text;
+            cliente.nombre = txtNombre.Text;
+            cliente.direccion = txtDireccion.Text;
+            cliente.telefono = txtTelefono.Text;
+            cliente.fecha_inscripcion = dtpFechaIngreso.Value.Date;
+
+
+            Cliente.AgregarCliente(cliente);
+            LimpiarFormulario();
+            ActualizarListaCliente();
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtDocumento.Text = "";
+            txtNombre.Text = "";
+            txtDireccion.Text = "";
+            txtTelefono.Text = "";
+            dtpFechaIngreso.Value = System.DateTime.Now;
+
+        }
+
+
+        private void ActualizarListaCliente()
+        {
+            lstCliente.DataSource = null;
+            lstCliente.DataSource = Cliente.ObtenerClientes();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            int index = lstCliente.SelectedIndex;
+            Cliente c = ObtenerClienteFormulario();
+            Cliente.EditarCliente(index, c);
+
+
+            LimpiarFormulario();
+            ActualizarListaCliente();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.lstCliente.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Favor seleccione una fila");
+            }
+            else
+            {
+                Cliente c = (Cliente)lstCliente.SelectedItem;
+                Cliente.EliminarCliente(c);
+                ActualizarListaCliente();
+                LimpiarFormulario();
+            }
+        }
+
+        private void frmCliente_Load(object sender, EventArgs e)
+        {
+            ActualizarListaCliente();
+        }
+
+
+        private Cliente ObtenerClienteFormulario()
+        {
+            Cliente cliente = new Cliente();
+            cliente.nroDocumento = txtDocumento.Text;
+            cliente.nombre = txtNombre.Text;
+            cliente.direccion = txtDireccion.Text;
+            cliente.telefono = txtTelefono.Text;
+            cliente.fecha_inscripcion = dtpFechaIngreso.Value.Date;
+            return cliente;
+        }
+
+
+        private void lstCliente_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cliente c = (Cliente)lstCliente.SelectedItem;
+
+            if (c != null)
+            {
+                txtDocumento.Text = Convert.ToString(c.nroDocumento);
+                txtNombre.Text = c.nombre;
+                txtDireccion.Text = c.direccion;
+                txtTelefono.Text = c.telefono;
+                dtpFechaIngreso.Value = c.fecha_inscripcion;
+            }
+
+
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
     }
 }
 
