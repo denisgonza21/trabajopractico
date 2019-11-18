0dbfa11cb21c826c0ec1635ef342e53d02858b88
PabloV97 <pablo.vera1997@gmail.com>
2019-11-17 23:10:53 -0300

    Realize la conexion de datos al sqlserver del formulario Funcionario

--- ClasesBiblioteca/Funcionario.cs
+++ ClasesBiblioteca/Funcionario.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using ClasesBiblioteca;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace ClasesBiblioteca
 {
@@ -14,8 +15,175 @@ namespace ClasesBiblioteca
         public string telefono { get; set; }
         public DateTime fechaInicio { get; set; }
         public string usuario { get; set; }
-
-
         
+
+
+        public static List<Funcionario> listafuncionario = new List<Funcionario>();
+
+        public static void AgregarFuncionario(Funcionario fun)
+        {
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string textoCmd = "insert into Funcionario (nroDocumento, nombre, direccion, telefono, fechaInicio, usuario) VALUES (@nroDocumento, @nombre, @direccion, @telefono, @fechaInicio, @usuario)";
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+                SqlParameter p1 = new SqlParameter("@nroDocumento", fun.nroDocumento);
+                SqlParameter p2 = new SqlParameter("@nombre", fun.nombre);
+                SqlParameter p3 = new SqlParameter("@direccion", fun.direccion);
+                SqlParameter p4 = new SqlParameter("@telefono", fun.telefono);
+                SqlParameter p5 = new SqlParameter("@fechaInicio", fun.fechaInicio);
+                SqlParameter p6 = new SqlParameter("@usuario", fun.usuario);
+
+
+
+                p1.SqlDbType = SqlDbType.VarChar;
+                p2.SqlDbType = SqlDbType.VarChar;
+                p3.SqlDbType = SqlDbType.VarChar;
+                p4.SqlDbType = SqlDbType.VarChar;
+                p5.SqlDbType = SqlDbType.DateTime;
+                p6.SqlDbType = SqlDbType.VarChar;
+
+
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2);
+                cmd.Parameters.Add(p3);
+                cmd.Parameters.Add(p4);
+                cmd.Parameters.Add(p5);
+                cmd.Parameters.Add(p6);
+
+
+
+                cmd.ExecuteNonQuery();
+
+            }
+
+
+
+
+        }
+
+        public static void EliminarFuncionario(Funcionario fun)
+        {
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string SENTENCIA_SQL = "delete from Funcionario where id = @id";
+
+                SqlCommand cmd = new SqlCommand(SENTENCIA_SQL, con);
+                SqlParameter p7 = new SqlParameter("@id", fun.id);
+                p7.SqlDbType = SqlDbType.Int;
+                cmd.Parameters.Add(p7);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void EditarFuncionario(int index, Funcionario fun)
+        {
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                string textoCMD = "UPDATE Funcionario SET nroDocumento = @nroDocumento, nombre = @nombre, direccion = @direccion, telefono = @telefono, fechaInicio = @fechaInicio, usuario = @usuario   where id = @id";
+
+                SqlCommand cmd = new SqlCommand(textoCMD, con);
+
+                SqlParameter p1 = new SqlParameter("@nroDocumento", fun.nroDocumento);
+                SqlParameter p2 = new SqlParameter("@nombre", fun.nombre);
+                SqlParameter p3 = new SqlParameter("@direccion", fun.direccion);
+                SqlParameter p4 = new SqlParameter("@telefono", fun.telefono);
+                SqlParameter p5 = new SqlParameter("@fechaInicio", fun.fechaInicio);
+                SqlParameter p6 = new SqlParameter("@usuario", fun.usuario);
+                SqlParameter p7 = new SqlParameter("@id", fun.id);
+
+
+                p1.SqlDbType = SqlDbType.VarChar;
+                p2.SqlDbType = SqlDbType.VarChar;
+                p3.SqlDbType = SqlDbType.VarChar;
+                p4.SqlDbType = SqlDbType.VarChar;
+                p5.SqlDbType = SqlDbType.DateTime;
+                p6.SqlDbType = SqlDbType.VarChar;
+                p7.SqlDbType = SqlDbType.Int;
+
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2);
+                cmd.Parameters.Add(p3);
+                cmd.Parameters.Add(p4);
+                cmd.Parameters.Add(p5);
+                cmd.Parameters.Add(p6);
+                cmd.Parameters.Add(p7);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static Funcionario ObtenerFuncionarios(int id)
+        {
+            Funcionario funcionario = null;
+
+            if (listafuncionario.Count == 0)
+            {
+                Funcionario.ObtenerFuncionario();
+            }
+
+            foreach (Funcionario fun in listafuncionario)
+            {
+                if (fun.id == id)
+                {
+                    funcionario = fun;
+                    break;
+                }
+            }
+
+            return funcionario;
+        }
+
+        public static List<Funcionario> ObtenerFuncionario()
+        {
+
+
+            Funcionario funcionario;
+            listafuncionario.Clear();
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string textoCMD = "Select * from Funcionario";
+
+                SqlCommand cmd = new SqlCommand(textoCMD, con);
+
+                SqlDataReader elLectorDeDatos = cmd.ExecuteReader();
+
+                while (elLectorDeDatos.Read())
+                {
+                    funcionario = new Funcionario();
+                    funcionario.id = elLectorDeDatos.GetInt32(0);
+                    funcionario.nroDocumento = elLectorDeDatos.GetString(1);
+                    funcionario.nombre = elLectorDeDatos.GetString(2);
+                    funcionario.direccion = elLectorDeDatos.GetString(3);
+                    funcionario.telefono = elLectorDeDatos.GetString(4);
+                    funcionario.fechaInicio = elLectorDeDatos.GetDateTime(5);
+                    funcionario.usuario = elLectorDeDatos.GetString(6);
+                    
+                 
+                    
+
+
+                    listafuncionario.Add(funcionario);
+                }
+
+                return listafuncionario;
+
+            }
+        }
+        public override string ToString()
+        {
+            return usuario;
+        }
     }
 }
--- ProyectoLibro/frmFuncionario.cs
+++ ProyectoLibro/frmFuncionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClasesBiblioteca;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,124 @@ namespace ProyectoLibro
             InitializeComponent();
         }
 
-        private void txtPassword_Click(object sender, EventArgs e)
+    
+
+        private void frmFuncionario_Load(object sender, EventArgs e)
         {
-            txtPassword.UseSystemPasswordChar = true;
+            ActualizarListaFuncionarios();
+            txtId.Enabled = false;
+        }
+
+        private void ActualizarListaFuncionarios()
+        {
+            lstFuncionario.DataSource = null;
+            lstFuncionario.DataSource = Funcionario.ObtenerFuncionario();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Funcionario fun = new Funcionario();
+            
+            fun.nroDocumento = txtNroDocumento.Text;
+            fun.nombre = txtNombre.Text;
+            fun.direccion = txtDireccion.Text;
+            fun.telefono = txtTelefono.Text;
+            fun.usuario = txtUsuario.Text;
+            fun.fechaInicio = dtpFechaIngreso.Value;
+           
+
+
+
+            Funcionario.AgregarFuncionario(fun);
+            LimpiarFormulario();
+            ActualizarListaFuncionarios();
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtId.Text = "";
+            txtNroDocumento.Text = "";
+            txtNombre.Text = "";
+            txtDireccion.Text = "";
+            txtTelefono.Text = "";
+            txtUsuario.Text = "";
+            dtpFechaIngreso.Value = System.DateTime.Now;
+            
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            Funcionario func = (Funcionario)lstFuncionario.SelectedItem;
+            if (func != null)
+            {
+                int index = lstFuncionario.SelectedIndex;
+                Funcionario funcionario = ObtenerFuncionarioOtro();
+                Funcionario.EditarFuncionario(index, func);
+
+
+                LimpiarFormulario();
+                ActualizarListaFuncionarios();
+            }
+            else
+            {
+                MessageBox.Show("Ojo, Selecciona un Item");
+            }
+        }
+
+        private Funcionario ObtenerFuncionarioOtro()
+        {
+            Funcionario fun = new Funcionario();
+            fun.id = Convert.ToInt16(txtId.Text);
+            fun.nroDocumento = txtNroDocumento.Text;
+            fun.nombre = txtNombre.Text;
+            fun.direccion = txtDireccion.Text;
+            fun.telefono = txtTelefono.Text;
+            fun.fechaInicio = dtpFechaIngreso.Value.Date;
+            fun.usuario = txtUsuario.Text;
+            return fun;
+        }
+
+        private void lstFuncionario_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Funcionario func = (Funcionario)lstFuncionario.SelectedItem;
+
+            if (func != null)
+            {
+                txtId.Text = Convert.ToString(func.id);
+                txtNroDocumento.Text = func.nroDocumento;
+                txtNombre.Text = func.nombre;
+                txtDireccion.Text = func.direccion;
+                txtTelefono.Text = func.telefono;
+                dtpFechaIngreso.Value = func.fechaInicio;
+                txtUsuario.Text = func.usuario;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Funcionario func = (Funcionario)lstFuncionario.SelectedItem;
+            if (func != null)
+            {
+                Funcionario funcionario = (Funcionario)lstFuncionario.SelectedItem;
+                Funcionario.EliminarFuncionario(funcionario);
+                ActualizarListaFuncionarios();
+                LimpiarFormulario();
+            }
+            else
+            {
+                MessageBox.Show("Ojo, Selecciona un Item");
+            }
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
